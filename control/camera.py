9d8c0cf3edb81c6f6b7578d812cca3e954cf3dfe
hlgirard <hlgirard@users.noreply.github.com>
2019-08-01 11:05:33 -0400

    Basic camera support

---
+++ control/camera.py
@@ -0,0 +1,26 @@
+import cv2
+
+class Camera:
+
+    def __init__(self):
+
+        # Select the first available camera device
+        self._cam = cv2.VideoCapture(0)
+
+        # Make sure camera is opened
+        if not self._cam.isOpened():
+            raise IOError("Failed to access camera. Make sure it is connected and powered on.")
+
+    def __del__(self):
+        self._cam.release()
+
+    def capture(self, savepath):
+        '''Capture a single image and save it to savepath'''
+
+        ret, frame = self._cam.read()
+
+        # ret is True if the frame was correctly received, False otherwise
+        if not ret:
+            raise IOError("Cannot receive frame. Make sure camera is on and connected.")
+
+        cv2.imwrite(savepath, frame)
